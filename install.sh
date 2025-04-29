790315cc46f031b95274d2333a181c3fcad37f28
shell-ninja <me.js.bro@gmail.com>
2025-04-30 00:44:09 +0600

    fixed

--- install.sh
+++ install.sh
@@ -75,7 +75,7 @@ gpgkey=https://repo.charm.sh/yum/gpg.key' | sudo tee /etc/yum.repos.d/charm.repo
     sudo yum install --assumeyes gum &> /dev/null
 
     if command -v gum &> /dev/null; then
-        printf "${cyan}::${end} Successfully installed ${cyan}gim${end}...\n"
+        printf "${cyan}::${end} Successfully installed ${cyan}gum${end}...\n"
     fi
 fi
 
